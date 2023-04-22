e3021fb53816c25a1ff792e58148ce33c04219b9
Elmigo <31411563+Elmigo@users.noreply.github.com>
2023-04-22 10:52:26 +0200

    Add files via upload

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+    private ObservableCollection<TestList> testCollection = new ObservableCollection<TestList>();
+
+    public MainPage()
+	{
+		InitializeComponent();
+
+        testCollection.Add(new TestList
+        {
+            string1 = "Welcome to .NET MAUI",
+            string2 = "Testing application"
+        });
+
+        testCollection.Add(new TestList
+        {
+            string1 = "Welcome to .NET MAUI",
+            string2 = "Testing application"
+        });
+
+        testCollection.Add(new TestList
+        {
+            string1 = "Welcome to .NET MAUI",
+            string2 = "Testing application"
+        });
+
+        testCollection.Add(new TestList
+        {
+            string1 = "Welcome to .NET MAUI",
+            string2 = "Testing application"
+        });
+
+        testCollection.Add(new TestList
+        {
+            string1 = "Welcome to .NET MAUI",
+            string2 = "Testing application"
+        });
+
+        testCollection.Add(new TestList
+        {
+            string1 = "Welcome to .NET MAUI",
+            string2 = "Testing application"
+        });
+
+        testCollection.Add(new TestList
+        {
+            string1 = "Welcome to .NET MAUI",
+            string2 = "Testing application"
+        });
+
+        testCollection.Add(new TestList
+        {
+            string1 = "Welcome to .NET MAUI",
+            string2 = "Testing application"
+        });
+
+        testCollection.Add(new TestList
+        {
+            string1 = "Welcome to .NET MAUI",
+            string2 = "Testing application"
+        });
+
+        testCollection.Add(new TestList
+        {
+            string1 = "Welcome to .NET MAUI",
+            string2 = "Testing application"
+        });
+
+        testView.ItemsSource = testCollection;
+    }
+}
---
+++ MauiApp1/TestList.cs
@@ -0,0 +1,8 @@
+﻿namespace MauiApp1
+{
+    public class TestList
+    {
+        public string string1 {  get; set; }
+        public string string2 { get; set; }
+    }
+}
